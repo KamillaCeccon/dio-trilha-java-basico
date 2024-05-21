8113f1e7eac3c54d631720353f5e2073b0d19b1d
KamiCeccon <kamiceccon@icloud.com>
2024-05-20 21:48:16 -0300

    meu projeto conta banco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+    public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+		System.out.println("Digite seu nome");
+		String nome = scanner.next();
+
+		System.out.println("Digite seu sobrenome");
+		String sobrenome = scanner.next();
+
+		System.out.println("Olá meu nome é: " + nome + " " + sobrenome);
+
+	}
+
+}
