e2cc50f14f9c308fdbfaf771d50b78f29be91c0a
KamiCeccon <kamiceccon@icloud.com>
2024-05-20 22:23:13 -0300

    commit projeto ok

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -5,16 +5,25 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
 
-		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-		System.out.println("Digite seu nome");
-		String nome = scanner.next();
+        System.out.println("Por favor, digite o número da Agência !");
+        String agenciaBanco = scanner.next();
 
-		System.out.println("Digite seu sobrenome");
-		String sobrenome = scanner.next();
+        System.out.println("Digite sua conta do banco, sem o digito");
+        Integer conta = scanner.nextInt();
 
-		System.out.println("Olá meu nome é: " + nome + " " + sobrenome);
+        System.out.println("Informe seu nome completo sem abreviações");
+        String nomeCliente = scanner.next();
+        scanner.nextLine();
 
-	}
+        System.out.println("Informe seu saldo");
+        Double saldoBancario = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agenciaBanco + ", conta " + conta + " e seu saldo " + saldoBancario + " já está disponível para saque");
+
+    }
 
 }
